5a5e6312f69e0dc0dc943e15ede46f7b12261824
David Bell <davidgbe@usc.edu>
2015-08-10 01:55:21 -0700

    add template for NeuralNet

---
+++ neural_net.cpp
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include <cstdlib>
+#include <iostream>
+
+#include "neural_net.h"
+
+namespace augur {
+
+  NeuralNet::NeuralNet(int* structure, int num_levels) {
+
+  }
+
+  NeuralNet::~NeuralNet() {
+
+  }
+
+  void NeuralNet::backpropagate_train(double* X, double* Y, int iterations) {
+
+  }
+
+  double NeuralNet::predict(double* X) {
+    return 1.0;
+  }
+
+
+}
---
+++ neural_net.h
@@ -0,0 +1,22 @@
+#ifndef NEURAL_NET_H
+#define NUERAL_NET_H
+
+#include "perceptron.h"
+
+namespace augur {
+  class NeuralNet {
+    public:
+      NeuralNet(int* structure, int num_levels);
+      ~NeuralNet();
+      void backpropagate_train(double* X, double* Y, int iterations);
+      double predict(double* X);
+
+    private:
+      std::string name;
+      int* stucture;
+      int num_levels;
+
+  };
+}
+
+#endif
